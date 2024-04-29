9e5f5868114f660ceddcb74482064e9c1edc88f0
Rahul Kumar <kumarrahul46700@gmail.com>
2024-04-29 22:02:54 +0530

    ROute authenticated

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -10,11 +10,11 @@ import { selectUserData } from '../pages/authantication/authSlice';
 
 export const APRoutes = () => {
   const ROUTES = [
-    { name: '/', value: (index) => (isLoggedIn ? <Home key={index} updatePageTitle={updatePageTitle} /> : <Auth key={index} updatePageTitle={updatePageTitle} />) },
-    { name: '/auth', value: (index) => (!isLoggedIn ? <Auth key={index} updatePageTitle={updatePageTitle} /> : <Home key={index} updatePageTitle={updatePageTitle} />) },
+    { name: '/', value: (index) => ( <Home key={index} updatePageTitle={updatePageTitle} />) },
+    { name: '/auth', value: (index) => (<Auth key={index} updatePageTitle={updatePageTitle} />) },
     { name: '/home', value: (index) => <Home key={index} updatePageTitle={updatePageTitle} /> },
     { name: '/home/:listId', value: (index) => <Item key={index} updatePageTitle={updatePageTitle} /> },
-    { name: '/postAd', value: (index) => (isLoggedIn ? <PostProperty key={index} updatePageTitle={updatePageTitle} /> : <PageNotFound updatePageTitle={updatePageTitle} />) },
+    { name: '/postAd', value: (index) => (<PostProperty key={index} updatePageTitle={updatePageTitle} /> ) },
   ];
   const isLoggedIn = useSelector(selectUserData);
   const [pageTitle, updatePageTitle] = useState('');
@@ -26,7 +26,7 @@ export const APRoutes = () => {
   return (
     <Routes>
       {ROUTES.map((route, index) => (
-        <Route path={route.name} element={route.value(index)} key={route.name} />
+        <Route path={route.name} element={ isLoggedIn ? route.value(index) :<Auth key={index} updatePageTitle={updatePageTitle} /> } key={route.name} /> 
       ))}
       <Route path="*" element={<PageNotFound updatePageTitle={updatePageTitle} />} /> {/* Catch-all route */}
     </Routes>
