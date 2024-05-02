5d266a0182b2006bda7e29245d1ba7ff2fb29a9b
Kuldeep Kumar <118204485+KuldeepKumarAngural@users.noreply.github.com>
2024-05-02 17:52:08 +0530

    ApDialogChanges

--- src/components/modal/APDialog.js
+++ src/components/modal/APDialog.js
@@ -1,9 +1,17 @@
-import { Box, Dialog } from '@mui/material';
+import { Box, Dialog , DialogContent , IconButton} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import React from 'react';
-export const APDialog = ({ open, close, content, sx }) => {
+export const APDialog = ({ open, close, content, sx ,disableClose =false}) => {
   return (
     <Dialog keepMounted className="test-class" maxWidth={'500'} open={open} onClose={close} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-      <Box style={{ ...sx }}> {content}</Box>
+      <Box style={{ ...sx }}>
+      <DialogContent >
+        {content}
+        {!disableClose &&<IconButton aria-label="close" onClick={close} sx={{ position: 'absolute', right: 8, top: 8, color: 'grey' }}>
+      <CloseIcon /></IconButton>}
+       </DialogContent>
+         
+         </Box>
     </Dialog>
   );
 };
