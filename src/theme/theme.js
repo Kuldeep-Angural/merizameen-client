e30115bd7ff7732cbf96b8d4e5fff4ca470e1dbf
Rahul Kumar <kumarrahul46700@gmail.com>
2024-05-22 12:14:47 +0530

    change in theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,33 +5,29 @@ export const theme = createTheme({
   palette: {
     primary: {
       main: '#45a148',
-      light: '#45a148',
+      light: '#82C984',
       dark: '#f5bc00',
-      citron: '#BBBE64',
-      magenta: '#8e5572',
-      mintcream: '#F2F7F2',
-      vista: '#7699D4',
     },
     secondary: {
-      main: '#f5bc00',
+      main: '#ffe180',
     },
     success: {
       main: green[200],
     },
     error: {
-      main: red[900],
+      main: red[700],
     },
     warning: {
-      main: orange[200],
+      main: orange[300],
     },
     info: {
-      main: blue[200],
+      main: blue[100],
     },
     text: {
       main: '#000000',
     },
     input: {
-      main: '#1B1A55',
+      main: '#514FC9',
     },
   },
 });
