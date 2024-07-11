c9b26818e50fff700789018b96d6980454408f3c
Kuldeep Kumar <118204485+Kuldeep-Angural@users.noreply.github.com>
2024-07-11 09:35:05 +0000

    again minor fixing

--- src/components/googleMap/GoogleMap.js
+++ src/components/googleMap/GoogleMap.js
@@ -5,15 +5,23 @@ export const GoogleMap = ({data}) => {
 
     return (
       <div className="mapouter">
-        <div className="gmap_canvas">
-          <iframe className="gmap_iframe" title="Google Map" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src={mapSrc}></iframe>
-        </div>
-        <style>{`
-          .mapouter { position: relative; text-align: right; width: 100%; height: 300px;}
-          .gmap_canvas { overflow: hidden; background: none!important; width: 100%; height: 300px; }
-          .gmap_iframe { width: 100% !important; height: 300px !important; }
-          .gmnoprint .gm-style-mtc { right: 10px !important; left: auto !important;}
-        `}</style>
+      <div className="gmap_canvas">
+        <iframe
+          className="gmap_iframe"
+          title="Google Map"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src={mapSrc}
+        ></iframe>
       </div>
+      <style>{`
+        .mapouter { position: relative; text-align: right; width: 100%; height: 200px;}
+        .gmap_canvas { overflow: hidden; background: none!important; width: 100%; height: 200px; }
+        .gmap_iframe { width: 100% !important; height: 200px !important; }
+        .gmnoprint .gm-style-mtc { right: 10px !important; left: auto !important;}
+      `}</style>
+    </div>
     );
   };
